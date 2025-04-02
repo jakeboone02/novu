78c219e3ed07cbb4f1a3dacb974b76f5b1a6254d
desiprisg <george.desipris.56@gmail.com>
2025-04-02 13:18:18 +0300

    feat(api): Require either subject or body for in-app

--- libs/application-generic/src/schemas/control/in-app-control.schema.ts
+++ libs/application-generic/src/schemas/control/in-app-control.schema.ts
@@ -33,17 +33,31 @@ const actionZodSchema = z
   })
   .optional();
 
-export const inAppControlZodSchema = z.object({
+// First, define the common properties that both schema variants will share
+const commonInAppProperties = {
   skip: skipZodSchema,
   disableOutputSanitization: z.boolean().optional(),
-  subject: z.string().optional(),
-  body: z.string(),
   avatar: z.string().regex(redirectUrlRegex).optional(),
   primaryAction: actionZodSchema,
   secondaryAction: actionZodSchema,
   data: z.object({}).catchall(z.unknown()).optional(),
   redirect: redirectZodSchema.optional(),
+};
+
+const subjectRequiredSchema = z.object({
+  subject: z.string(),
+  body: z.string().optional(),
+  ...commonInAppProperties,
 });
+
+const bodyRequiredSchema = z.object({
+  subject: z.string().optional(),
+  body: z.string(),
+  ...commonInAppProperties,
+});
+
+// Write it this way because of how translation from zod to json schema works
+export const inAppControlZodSchema = z.union([subjectRequiredSchema, bodyRequiredSchema]);
 
 export type InAppRedirectType = z.infer<typeof redirectZodSchema>;
 export type InAppActionType = z.infer<typeof actionZodSchema>;
